4e3ae6eb8f37c5d8799abdb5f2d61bd70f344383
smeerws <hello@smeerws.at>
2019-02-06 07:18:25 +0100

    code for simple singleton

---
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+
+    private InputField objName;
+    private TextMeshProUGUI tmpName;
+
+    public SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        objName = FindObjectOfType<InputField>();
+    }
+
+    public void SaveName()
+    {
+        SimpleSingleton.instance.Name = objName.text;
+        sceneLoader.LoadNextScene();
+    }
+
+    public void LoadName()
+    {
+        if(tmpName == null)
+        {
+            tmpName = GameObject.Find("TMPName").GetComponent<TextMeshProUGUI>();
+        }
+  
+        tmpName.text = SimpleSingleton.instance.Name;
+    }
+}
---
+++ Assets/MyGame/Scripts/SimpleSingleton.cs
@@ -0,0 +1,43 @@
+﻿/* This is the simplest singleton implementation, but it has some issues:
+ * 1) Singleton is not persistent across the Unity scenes.
+ * 2) All the executable code must be attached to GameObject in the hierarchy.
+ * 3) Whenever we need a class to be a singlton we have to copy the singleton mechanic in Awake().
+ */
+ using UnityEngine;
+
+public class SimpleSingleton : MonoBehaviour {
+
+    private readonly string Tag = "simpleSingleton";
+    private string yourName = "empty";
+
+    public static SimpleSingleton instance;
+
+    public string Name
+    {
+        get
+        {
+            Debug.Log(Tag + " get name " + yourName);
+            return yourName;
+        }
+
+        set
+        {
+            yourName = value;
+            Debug.Log(Tag + " name set to " + yourName);
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Destroy " + Tag + " " + gameObject.name);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("Assign this " + Tag + " instance  with name " + gameObject.name);
+            instance = this;
+        }
+    }
+}
